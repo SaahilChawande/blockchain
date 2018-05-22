90cf216b0d7afa3161aebef82175d6139f6659ac
SaahilChawande <chawandesaahil600@gmail.com>
2018-05-23 01:06:48 +0530

    Testing Create New Transaction Method

--- dev/test.js
+++ dev/test.js
@@ -2,8 +2,16 @@ const Blockchain = require('./blockchain');
 
 const bitcoin = new Blockchain();
 
-bitcoin.createNewBlock(1234, 'SFXGFGDHD05', 'SFGLDJGNLKJ67J5J');
-bitcoin.createNewBlock(23443, 'SFGFHG45764', 'GHBGFBNCGHG456');
-bitcoin.createNewBlock(5476, 'SDGXFGDXFG4767', 'DVGFXGVDRF53645');
+bitcoin.createNewBlock(2434, 'SDSFFGNLJN45355', 'DFKDSNSLFDJN8ETG39');
 
-console.log(bitcoin);
+bitcoin.createNewTransaction(100, 'ALEX_VFSKV365K', 'JEN_JRGL3566');
+
+bitcoin.createNewBlock(23434, 'DLVSLWTLKW', 'SDFLKNETN');
+
+bitcoin.createNewTransaction(5453, 'ALEX_VFSKV365K', 'JEN_JRGL3566');
+bitcoin.createNewTransaction(3454, 'ALEX_VFSKV365K', 'JEN_JRGL3566');
+bitcoin.createNewTransaction(3465, 'ALEX_VFSKV365K', 'JEN_JRGL3566');
+
+bitcoin.createNewBlock(23543465, 'DLVSLWTLKW', 'SDFLKNETN');
+
+console.log(bitcoin.chain[2]);
