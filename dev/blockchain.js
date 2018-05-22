02c7a6c70784a9a3246ae1a65fd65b34796324fe
SaahilChawande <chawandesaahil600@gmail.com>
2018-05-22 16:13:17 +0530

    Get Last Block Method

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -19,6 +19,10 @@ Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash)  
     return newBlock;
 };
 
+Blockchain.prototype.getLastBlock = function()  {
+    return this.chain[this.chain.length - 1];
+}
+
 
 
 module.exports = Blockchain;
