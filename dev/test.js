91936c0516bcdf59b51a682d5c9cb8907fad0c4b
SaahilChawande <chawandesaahil600@gmail.com>
2018-05-25 16:50:10 +0530

    Creating A Genesis Block

--- dev/test.js
+++ dev/test.js
@@ -1,24 +1,4 @@
 const Blockchain = require('./blockchain');
 const bitcoin = new Blockchain();
 
-const prevoiusBlockHash = 'LXVNLFGNLKMNEL5N6';
-const currentBlockData = [
-    {
-        amount: 10,
-        sender: 'XLFKVNFLKNELKTN',
-        recipient: 'SLDVNLNLJNTLJENR'
-    },
-    {
-        amount: 31,
-        sender: 'XLFKVNFLKNELKTN',
-        recipient: 'SLDVNLNLJNTLJENR'
-    },
-    {
-        amount: 56,
-        sender: 'XLFKVNFLKNELKTNM',
-        recipient: 'SLDVNLNLJNTLJENR'
-    }
-];
-
-// console.log(bitcoin.proofOfWork(prevoiusBlockHash, currentBlockData));
-console.log(bitcoin.hashBlock(prevoiusBlockHash, currentBlockData, 33979));
+console.log(bitcoin);
