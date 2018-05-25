373acf8341153e4177662d2139b2001aae7886e2
SaahilChawande <chawandesaahil600@gmail.com>
2018-05-25 18:56:38 +0530

    Building The API Foundation

--- dev/api.js
+++ dev/api.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
-    res.send('Hello World');
+app.get('/blockchain', function(req, res) {
+
 });
 
-app.listen(3000);
+app.post('/transaction', function(req, res) {
+
+});
+
+app.get('/mine', function(req, res) {
+
+});
+
+app.listen(3000, function() {
+    console.log('Listening on port 3000...');
+});
