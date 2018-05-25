5419453a9e34e1df98594401839f13289bb1ebd9
SaahilChawande <chawandesaahil600@gmail.com>
2018-05-25 19:22:00 +0530

    Installing Postman and Body-parser

--- dev/api.js
+++ dev/api.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : false }));
 
 app.get('/blockchain', function(req, res) {
 
 });
 
 app.post('/transaction', function(req, res) {
-
+    console.log(req.body);
+    res.send(`The amount of the transaction is ${req.body.amount} bitcoin`);
 });
 
 app.get('/mine', function(req, res) {
