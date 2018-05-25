66e04d5236b57e0915fe079ebb45e78389086d85
SaahilChawande <chawandesaahil600@gmail.com>
2018-05-25 20:03:10 +0530

    Building The "GET/mine" Endpoint

--- dev/api.js
+++ dev/api.js
@@ -2,23 +2,42 @@ var express = require('express');
 var app = express();
 const bodyParser = require('body-parser');
 const Blockchain = require('./blockchain');
+const uuid = require('uuid/v1');
+
+const nodeAddress = uuid().split('-').join('');
 
 const bitcoin = new Blockchain();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : false }));
 
+// get the entire blockchain
 app.get('/blockchain', function(req, res) {
     res.send(bitcoin);
 });
 
+// make a new transaction
 app.post('/transaction', function(req, res) {
     const blockIndex = bitcoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
     res.json({ note: `Transaction will be added in block ${blockIndex}.`});
 });
 
+// mine a block
 app.get('/mine', function(req, res) {
+    const lastBlock = bitcoin.getLastBlock();
+    const previousBlockHash = lastBlock['hash'];
+    const currentBlockData = {
+        transactions: bitcoin.pendingTransactions,
+        index: lastBlock['index'] + 1
+    };
+    const nonce = bitcoin.proofOfWork(previousBlockHash, currentBlockData);
+    const blockHash = bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce);
 
+    // reward for mining a new block
+    bitcoin.createNewTransaction(12.5, "00", nodeAddress);
+
+    const newBlock = bitcoin.createNewBlock(nonce, previousBlockHash, blockHash);
+    res.json({ note: "New Block mined successfully", block: newBlock });
 });
 
 app.listen(3000, function() {
