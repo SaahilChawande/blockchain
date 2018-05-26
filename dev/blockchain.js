de672b8d75d97fc35507f017a11a0f2aea11400f
SaahilChawande <chawandesaahil600@gmail.com>
2018-05-26 19:32:54 +0530

    Creating Multiple Nodes

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -57,7 +57,6 @@ Blockchain.prototype.proofOfWork = function(previousBlockHash, currentBlockData)
     while(hash.substring(0, 4) !== '0000')  {
         nonce++;
         hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
-        console.log(hash);
     }
     return nonce;
 };
