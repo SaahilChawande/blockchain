42a9d6c7d750f70176a85554d595eee2061550b1
SaahilChawande <chawandesaahil600@gmail.com>
2018-05-31 19:31:39 +0530

    Refactoring "POST /transaction" Endpoint

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -20,8 +20,9 @@ app.get('/blockchain', function(req, res) {
 
 // make a new transaction
 app.post('/transaction', function(req, res) {
-    const blockIndex = bitcoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
-    res.json({ note: `Transaction will be added in block ${blockIndex}.`});
+    const newTransaction = req.body;
+    const blockIndex = bitcoin.addTransactionToPendingTransactions(newTransaction);
+    res.json({ note: `Transaction will be added in block ${blockIndex}.` });
 });
 
 //broadcast the transaction to all the network nodes
