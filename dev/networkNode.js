03385caf5f9d3e3a63185c3cf7f82a02fbcbe249
SaahilChawande <chawandesaahil600@gmail.com>
2018-05-31 19:25:33 +0530

    Building "POST /transaction/broadcast" Endpoint

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -24,6 +24,28 @@ app.post('/transaction', function(req, res) {
     res.json({ note: `Transaction will be added in block ${blockIndex}.`});
 });
 
+//broadcast the transaction to all the network nodes
+app.post('/transaction/broadcast', function(req, res)   {
+    const newTransaction = bitcoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
+    bitcoin.addTransactionToPendingTransactions(newTransaction);
+
+    const requestPromises = [];
+    bitcoin.networkNodes.forEach(networkNodeUrl =>  {
+        const requestOptions = {
+            uri: networkNodeUrl + '/transaction',
+            method: 'POST',
+            body: newTransaction,
+            json: true
+        };
+        requestPromises.push(rp(requestOptions));
+    });
+
+    Promise.all(requestPromises)
+        .then(data => {
+            res.json({ note: 'Transaction created and broadcast successful.' });
+        });
+});
+
 // mine a block
 app.get('/mine', function(req, res) {
     const lastBlock = bitcoin.getLastBlock();
