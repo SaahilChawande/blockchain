66cadde4b807461e952b9e2c5873a156e77fc1b8
SaahilChawande <chawandesaahil600@gmail.com>
2018-06-04 11:29:50 +0530

    Get Transaction Method

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -105,4 +105,18 @@ Blockchain.prototype.getBlock = function(blockHash)  {
     return correctBlock;
 };
 
+Blockchain.prototype.getTransaction = function(transactionId)   {
+    let correctTransaction = null;
+    let correctBlock = null;
+    this.chain.forEach(block => {
+        block.transactions.forEach(transaction => {
+            if (transaction.transactionId === transactionId) {
+                correctTransaction = transaction;
+                correctBlock = block;
+            }
+        });
+    });
+    return { transaction: correctTransaction, block: correctBlock};
+};
+
 module.exports = Blockchain;
