fd219b2c1c7f53423aa5da1c609f85c3f0ed6c61
SaahilChawande <chawandesaahil600@gmail.com>
2018-06-04 11:42:45 +0530

    Get Address Data Method

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -119,4 +119,24 @@ Blockchain.prototype.getTransaction = function(transactionId)   {
     return { transaction: correctTransaction, block: correctBlock};
 };
 
+Blockchain.prototype.getAddressData = function(address) {
+    const addressTransactions = [];
+    this.chain.forEach(block => {
+        block.transactions.forEach(transaction => {
+            if (transaction.sender === address || transaction.recipient === address)
+                addressTransactions.push(transaction);
+        });
+    });
+
+    let balance = 0;
+    addressTransactions.forEach(transaction => {
+        if (transaction.recipient === address)
+            balance += transaction.amount;
+        else if (transaction.sender === address)
+            balance -= transaction.amount;
+    });
+
+    return { addressTransactions: addressTransactions, addressBalance: balance };
+};
+
 module.exports = Blockchain;
